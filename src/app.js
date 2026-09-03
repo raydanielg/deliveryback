eb8a3a5aec0dd3bb60f41e38510fdfcfc42f02ca
raydanielg <airezra2+1@gmail.com>
2026-09-03 17:01:03 +0300

    cors: allow xerinexpress.com domain for frontend deployment

--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ const app = express()
 
 app.set("trust proxy", 1)
 
-const allowedOrigins = (process.env.CLIENT_URL || "http://localhost:3000")
+const allowedOrigins = (process.env.CLIENT_URL || "http://localhost:3000,https://xerinexpress.com,https://www.xerinexpress.com")
   .split(",")
   .map((o) => o.trim())
 
